574b4fe64a80fa7e95158ad8befa9226bf302d93
Koen <k.roosa@student.alfa-college.nl>
2019-05-20 14:03:22 +0200

    Bestellingen toegevoegd

--- src/Controller/BestellingenController.php
+++ src/Controller/BestellingenController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Bestelling;
+use App\Entity\Gerecht;
+use App\Entity\MenuItem;
 use App\Entity\Reservering;
+use App\Entity\Subgerecht;
+use phpDocumentor\Reflection\Types\Array_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,13 +22,126 @@ class BestellingenController extends AbstractController
      */
     public function index(Request $request)
     {
+        $menuItem1Sub1 = array();
+        $menuItem1Sub2 = array();
+        $menuItem2Sub1 = array();
+        $menuItem2Sub2 = array();
+        $menuItem2Sub3 = array();
+        $menuItem3Sub1 = array();
+        $menuItem3Sub2 = array();
+        $menuItem4 = array();
+        $menuItem5 = array();
+        $menuItem6 = array();
+        $menuItem7 = array();
+        $menuItem8 = array();
+        $menuItem9 = array();
+
         $id = $request->get('id');
+        //Reservering ophalen
+        $reservering = $this->getDoctrine()
+            ->getRepository(Reservering::class)
+            ->find($id);
+        //Menu items ophalen
+        $menuItem = $this->getDoctrine()
+            ->getRepository(MenuItem::class)
+            ->findAll();
+        //Foreach door alle menu items
+        foreach ($menuItem as $item){
+            switch ($item->getMenuItemcode()){
+                case '1':
+                    if ($item->getSubgerechtCode() == '1'){
+                        array_push($menuItem1Sub1, $item);
+                    }
+                    elseif ($item->getSubgerechtCode() == '2'){
+                        array_push($menuItem1Sub2, $item);
+                    }
+                    break;
+                case '2':
+                    if ($item->getSubgerechtCode() == '1'){
+                        array_push($menuItem2Sub1, $item);
+                    }
+                    elseif ($item->getSubgerechtCode() == '2'){
+                        array_push($menuItem2Sub2, $item);
+                    }
+                    elseif ($item->getSubgerechtCode() == '3'){
+                        array_push($menuItem2Sub3, $item);
+                    }
+                    break;
+                case '3':
+                    if ($item->getSubgerechtCode() == '1'){
+                        array_push($menuItem3Sub1, $item);
+                    }
+                    elseif ($item->getSubgerechtCode() == '2'){
+                        array_push($menuItem3Sub2, $item);
+                    }
+                    break;
+                case '4':
+                    array_push($menuItem4, $item);
+                    break;
+                case '5':
+                    array_push($menuItem5, $item);
+                    break;
+                case '6':
+                    array_push($menuItem6, $item);
+                    break;
+                case '7':
+                    array_push($menuItem7, $item);
+                    break;
+                case '8':
+                    array_push($menuItem8, $item);
+                    break;
+                case '9':
+                    array_push($menuItem9, $item);
+                    break;
+                default:
+                    $error = 'Er is geen gerecht gevonden met de desbetreffende code...';
+                    break;
+
+            }
+        }
+
+        //Return staement
+        return $this->render('bestellingen/index.html.twig', [
+            'Reservering' => $reservering,
+            'menuItem1sub1' => $menuItem1Sub1,
+            'MenuItem1sub2' => $menuItem1Sub2,
+            'MenuItem2sub1' => $menuItem2Sub1,
+            'MenuItem2sub2' => $menuItem2Sub2,
+            'MenuItem2sub3' => $menuItem2Sub3,
+            'MenuItem3sub1' => $menuItem3Sub1,
+            'MenuItem3sub2' => $menuItem3Sub2,
+            'MenuItem4' => $menuItem4,
+            'MenuItem5' => $menuItem5,
+            'MenuItem6' => $menuItem6,
+            'MenuItem7' => $menuItem7,
+            'MenuItem8' => $menuItem8,
+            'MenuItem9' => $menuItem9,
+        ]);
+    }
+
+    /**
+     * @Route("/bestellingen/confirm", name="bestellingen_confirm")
+     */
+    public function confirm(Request $request)
+    {
+        $id = $request->get('id');
+        //Reservering ophalen
         $reservering = $this->getDoctrine()
             ->getRepository(Reservering::class)
             ->find($id);
 
-        return $this->render('bestellingen/index.html.twig', [
-            'Reservering' => $reservering,
+        if ($request->isMethod('post')){
+            foreach ($_POST as $item){
+                $bestelling = new Bestelling();
+
+            }
+        }
+
+        $cookies = $request->cookies->all();
+        return $this->render('bestellingen/confirm.html.twig', [
+            'data' => $cookies
         ]);
+
     }
+
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Reservering;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -11,8 +12,18 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request)
     {
+// unset cookies
+        if (isset($_SERVER['HTTP_COOKIE'])) {
+            $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+            foreach ($cookies as $cookie) {
+                $parts = explode('=', $cookie);
+                $name = trim($parts[0]);
+                setcookie($name, '', time() - 1000);
+                setcookie($name, '', time() - 1000, '/');
+            }
+        }
         $tafels = $this->getDoctrine()
             ->getRepository(Reservering::class)
             ->findAll();
